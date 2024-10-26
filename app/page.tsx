4a134fcdf8aee7b724cb42e80ecb2ae8ae474b1b
JuanmeGarcia <jmgcasd@gmail.com>
2024-10-26 16:26:44 -0300

    Theming for dev overflow done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Dev Overflow</h1>
+      <h1 className="h1-bold background-light850_dark100">Dev Overflow</h1>
       <p>Stack Overflow Clone</p>
     </div>
   );
